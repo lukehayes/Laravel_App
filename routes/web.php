7677a090524d1f83edea194a2a8f201859bdaada
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-06 09:55:53 +0100

    Added BookController resource route.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,5 @@ use App\Http\Controllers\FormController;
 
 Route::get('/', [FormController::class, 'index']);
 Route::get('/create', [FormController::class, 'create']);
-Route::post('/create', [FormController::class, 'store']);
+
+Route::resource('books', BookController::class);
