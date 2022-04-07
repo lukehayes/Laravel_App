77607c861a711591e6e0d2cb070db94294ebfaef
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-07 09:29:33 +0100

    Added simple nav routes to main layout.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,12 @@
         <title>App Name - @yield('title')</title>
     </head>
     <body>
+
+        <nav>
+            <a href="{{ route('books.index')}}">Books</a>
+            <a href="{{ route('books.create')}}">New Book</a>
+        </nav>
+
         @section('sidebar')
             This is the master sidebar.
         @show
