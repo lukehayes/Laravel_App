2e1d482a24e8372fe62a0be187ad76497b8c2b65
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-07 09:30:20 +0100

    Changed placeholder name for create book form.

--- resources/views/Books/create.blade.php
+++ resources/views/Books/create.blade.php
@@ -11,7 +11,8 @@
 @section('content')
     
 <form action="{{route('books.store')}}" method="POST">
-    <input type="text" placeholder="name" name="title">
+    @csrf
+    <input type="text" placeholder="title" name="title">
     <input type="text" placeholder="language" name="language">
     <input type="submit" value="Create New Book">
 </form>
