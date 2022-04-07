45a0865cef5eac957668af31ca4cfe94865d3c1f
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-07 09:30:41 +0100

    Added CRUD options to book.index.

--- resources/views/Books/index.blade.php
+++ resources/views/Books/index.blade.php
@@ -14,6 +14,11 @@
         <div class="book">
             {{ $book->title }}
             <span style="font-weight: bold;">{{ $book->language }}</span>
+            <div class="options" style='display: inline;'>
+                <a href="{{ route('books.show', [$book->id]) }}">Show</a>
+                <a href="{{ route('books.destroy', [$book->id]) }}">Destroy</a>
+                <a href="{{ route('books.destroy', [$book->id]) }}">{{ route('books.destroy', [$book->id])}}</a>
+            </div>
         </div>
     @endforeach()
 @endsection
