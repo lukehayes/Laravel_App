be4672878cf60a479a0912f9d5c7ff0c8e4d1dae
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-18 17:28:30 +0100

    Moved front page specific styles over to _front.scss

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>App Name - @yield('title')</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body class="front-page">
+		@yield('hero')
+		@yield('content')
+    </body>
+</html>
