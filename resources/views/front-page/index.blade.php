800e6c9a55cab56f04272b3937d0544a4b73ead5
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-21 10:37:50 +0100

    Moved navigation buttons to hero section of template.

--- resources/views/front-page/index.blade.php
+++ resources/views/front-page/index.blade.php
@@ -2,19 +2,19 @@
 
 @section('hero')
 	<div class="container">
-		<h3>Application</h3>
-		<p>Front facing application</p>
+		<h5>Application</h5>
+
+		<nav>
+			<a href="{{ route('login-index') }}">Login</a>
+			<a href="{{ route('dashboard-index') }}">Dashboard</a>
+		</nav>
+
 	</div>
 @endsection
 
 @section('content')
 	
 <div class="grid-container">
-
-<nav>
-	<a href="{{ route('login-index') }}">Login</a>
-	<a href="{{ route('dashboard-index') }}">Dashboard</a>
-</nav>
 
 <div class="grid-2">
 	<div class="content">
@@ -32,22 +32,6 @@
 	<div class="content">
 		<p>Mollitia fugit eum Provident quia qui ex tempore illum Debitis velit autem dolore consectetur libero. Itaque voluptate qui voluptatibus consequuntur minima?Dolor fuga dolorem libero ab ea? Suscipit autem aliquid ullam similique aut. Ullam asperiores nobis quia repudiandae ratione eligendi Fugiat delectus itaque fugit voluptate deserunt. Ea nulla ullam placeat reiciendis!</p>
 	</div>
-</div>
-
-<div class="grid-3">
-	<div class="content">
-		<p> Mauris accumsan scelerisque nunc, a sodales sem tincidunt eget. Maecenas scelerisque dolor dolor, id pharetra sapien convallis nec. Praesent sagittis dictum diam. Sed convallis ligula quis metus congue maximus. Nullam tristique velit sed nisl consectetur blandit. Mauris luctus est ac lectus varius, nec dictum dui ullamcorper. Nullam sed mollis arcu. In dui sapien, porta sit amet neque sit amet, gravida finibus tortor. Mauris et ipsum sed leo consectetur placerat. Suspendisse efficitur ipsum vel ultricies vehicula.
-Aenean ultrices venenatis auctor. Etiam luctus diam id consequat accumsan. Nulla venenatis, nunc ut congue gravida, magna eros mollis lectus, ac volutpat dui arcu et felis. Pellentesque et consectetur leo. Curabitur porttitor, dolor eget maximus dictum, erat lorem vestibulum ante, nec semper leo mi non odio. Vivamus eget eros consequat, elementum nisi vitae, finibus metus. Cras dictum aliquam elementum. In quis nibh odio. </p>
-	</div>
-
-	<div class="content">
-	<p>Fusce mollis fermentum dictum. Morbi hendrerit ante ac quam sodales, a aliquam sapien venenatis. Nam iaculis porta libero eget dignissim. Vestibulum malesuada dolor et felis viverra viverra. Nunc sollicitudin felis turpis, sed bibendum tortor pharetra eu. Donec quis placerat diam. Curabitur aliquam congue lorem, auctor laoreet ligula aliquet eu. Suspendisse ut fringilla quam. Donec ipsum justo, varius eget arcu vel, molestie fermentum dui. Nunc sagittis lorem vitae imperdiet sagittis. Morbi lacus magna, dignissim a odio sed, pulvinar eleifend ante. Duis leo erat, aliquet non orci quis, porta laoreet libero. Maecenas libero lorem, consequat quis ligula in, maximus aliquam felis. Proin suscipit orci vulputate diam ultricies, vitae gravida urna mollis. Curabitur id sagittis velit, quis suscipit velit. In odio massa, ullamcorper non dictum vestibulum, iaculis eu sapien. </p>
-	</div>
-
-	<div class="content">
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate erat nec augue varius euismod vel accumsan justo. Donec congue facilisis mi ac dignissim. Proin interdum, lectus non tincidunt consectetur, lectus nunc imperdiet justo, vitae posuere justo diam ac sapien. Mauris luctus laoreet erat, ut egestas dui auctor et. Vivamus porta justo a nulla ornare dapibus. Fusce dolor massa, aliquam quis sollicitudin quis, sollicitudin vitae quam. Sed dolor nulla, viverra in viverra non, congue a arcu. Nunc ornare purus non elit iaculis maximus. Mauris scelerisque nunc augue. </p>
-	</div>
-
 </div>
 </div>
 
