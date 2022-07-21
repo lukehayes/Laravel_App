d572a11557a2e284e9f8c483c0605935b5d5d5da
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-21 10:10:09 +0100

    Added simple-css import to front page.

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,10 +1,13 @@
 <html>
     <head>
         <title>App Name - @yield('title')</title>
+        <link rel="stylesheet" href="https://cdn.simplecss.org/simple.css">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
+
     <body class="front-page">
 		@yield('hero')
 		@yield('content')
     </body>
+
 </html>
